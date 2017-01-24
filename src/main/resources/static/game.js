c5e5e8a38715c60ac6286b23f733655391a4f96a
pedroalegre <alegre.p@gmail.com>
2017-01-24 17:14:34 +0100

    Module 2 - Task 3.3 Implement the HTML interface to place and submit ships (not finished)

--- src/main/resources/static/game.js
+++ src/main/resources/static/game.js
@@ -1,8 +1,92 @@
 var columnHeader = ["", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
 var rowHeader = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 
-drawGrid();
+var ships = [{
+		"shipType": "Carrier",
+		"length": 5,
+		"shipLocations": []
+	}, {
+		"shipType": "Battleship",
+		"length": 4,
+		"shipLocations": []
+	}, {
+		"shipType": "Submarine",
+		"length": 3,
+		"shipLocations": []
+	}, {
+		"shipType": "Destroyer",
+		"length": 3,
+		"shipLocations": []
+	}, {
+		"shipType": "PatrolBoat",
+		"length": 2,
+		"shipLocations": []
+}];
 
+$(document).ready(function ($) {
+	drawGrid();
+
+	// get the cellId on mouseover
+    /*$('.cell').mouseup(function() {
+       console.log(this.id);
+    });*/
+
+	$("#logoutform").submit(function (event) {
+		event.preventDefault();
+		$.ajax({
+			timeout: 1000,
+			type: 'POST',
+			url: '/api/logout'
+
+		}).done(function(data, textStatus, jqXHR) {
+			window.location.replace("/games.html");
+			alert("Leaving so soon?");
+		});
+	});
+
+	$("#confirmButton").hide();
+	$("#cancelButton").hide();
+	$("#positionButton").hide();
+
+	$("#addShipsButton").on("click", function(event) {
+		event.preventDefault();
+		$(".salvoesGrid").hide();
+		$("#addShipsButton").hide();
+		$("#confirmButton").show();
+		$("#cancelButton").show();
+		$("#positionButton").show();
+		addShips();
+	})
+	// Add ships to the repository and grid
+	$("#confirmButton").on("click", function(event) {
+		event.preventDefault();
+		$.post( {
+			url: "/api/games/players/" + getGamePlayerPath(location.search).gp + "/ships",
+			data: JSON.stringify(ships),
+			contentType: "application/json",
+
+		}).done(function(data, textStatus, jqXHR) {
+			$(".salvoesGrid").show();
+			window.location.reload();
+
+		}).fail(function(jqXHR, textStatus, errorThrown) {
+			console.log("shit!");
+			window.location.reload();
+		});
+	})
+
+	$("#cancelButton").on("click", function(event) {
+		event.preventDefault();
+		$(".salvoesGrid").show();
+		$("#addShipsButton").show();
+		$(".shipsList").hide();
+		$("#confirmButton").hide();
+		$("#cancelButton").hide();
+		$("#positionButton").hide();
+	})
+});
+
+// draw the grids
 function drawGrid() {
 
     createHeader();
@@ -31,7 +115,7 @@ function createRow() {
             if(j==0) {
                 row.append("<td class='rowHeader'>" + rowHeader[i] + "</td>");
             } else {
-                row.append("<td class='cell' id='" + rowHeader[i] + columnHeader[j] + "'></td>");
+                row.append("<td class='cell' ondrop='drop(event, this)' ondragover='allowDrop(event)' id='" + rowHeader[i] + columnHeader[j] + "'></td>");
             }
         });
 
@@ -108,34 +192,73 @@ function playersTitle() {
     $(".playerInfo").append("<div id=player2>");
 }
 
-$(document).ready(function ($) {
-	$("#logoutform").submit(function (event) {
-		event.preventDefault();
-		$.ajax({
-			timeout: 1000,
-			type: 'POST',
-			url: '/api/logout'
+function addShips() {
+	$(".shipsList").empty();
+	var shipsList = $("<ul class=shipsList></ul>");
+	$.each(ships, function(i) {
+		shipsList.append("<li class=" + ships[i].shipType + ">" + ships[i].shipType + "</li>");
+		shipsList.append("<li class=shipBlock draggable=true ondragstart='drag(event)' id=" + ships[i].shipType + "></li>");
+		$(".placeShips").append(shipsList);
+		$("#" + ships[i].shipType).css("width", 40 * ships[i].length + 3.6 * ships[i].length + "px");
+	});
+}
 
-		}).done(function(data, textStatus, jqXHR) {
-			window.location.replace("/games.html");
-			alert("Leaving so soon?");
-		});
+function allowDrop(event) {
+	event.preventDefault();
+}
+
+function drag(event) {
+	event.dataTransfer.setData("shipType", event.target.id);
+}
+
+// Paint the ships on the grid
+function drop(event) {
+	event.preventDefault();
+	var shipType = event.dataTransfer.getData("shipType");
+	var shipLength;
+	var cellId = event.target.id;
+	var letter = cellId.substr(0,1);
+	var number = Number(cellId.substr(1,2));
+	var shipTemp = [];
+	var letters;
+
+	$.each(ships, function(i) {
+		if(ships[i].shipType == shipType) {
+			shipLength = ships[i].length;
+		}
 	});
 
-	$("#addShips").submit(function (event) {
-		event.preventDefault();
-		$.post( {
-			url: "/api/games/players/" + getGamePlayerPath(location.search).gp + "/ships",
-			data: JSON.stringify([{shipType: "destroyer", shipLocations: ["A1", "B1", "C1"]}]),
-			contentType: "application/json",
+	for(i = 0; i < shipLength; i++) {
 
-		}).done(function(data, textStatus, jqXHR) {
-			window.location.reload();
-			console.log("fucking master!");
+		// Horizontal ships
+		if($('input[name=position]:checked').val() == "horizontal") {
+			horizontalShip(number, letter, shipLength, shipTemp);
 
-		}).fail(function(jqXHR, textStatus, errorThrown) {
-			console.log("shit!");
-		});
-	})
+		// Vertical ships
+		} else {
+			verticalShip(number, letter, shipLength, shipTemp);
+		}
+	}
+}
 
-});
+function horizontalShip(number, letter, shipLength, shipTemp) {
+	if(number + shipLength > 10) {
+	} else {
+		shipTemp.push(letter + (number + i));
+		$("#" + (letter + (number + i))).addClass("hasShip");
+	}
+}
+
+function verticalShip(number, letter, shipLength, shipTemp) {
+	letters = rowHeader.indexOf(letter);
+
+	if(letters + shipLength > 10) {
+	} else {
+		shipTemp.push(rowHeader[letters + i] + number);
+		$("#" + (rowHeader[letters + i] + number)).addClass("hasShip");
+	}
+}
+
+function hasShip() {
+
+}
